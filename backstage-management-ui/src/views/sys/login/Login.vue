cdbcbb42f67d20a8f5ef272e1188196ffe9ddb99
zhaoweijie <949210784@qq.com>
2022-03-28 12:16:52 +0800

    修改前端项目名称

--- backstage-management-ui/src/views/sys/login/Login.vue
+++ backstage-management-ui/src/views/sys/login/Login.vue
@@ -35,8 +35,8 @@
             class="relative w-full px-5 py-8 mx-auto my-auto rounded-md shadow-md xl:ml-16 xl:bg-transparent sm:px-8 xl:p-4 xl:shadow-none sm:w-3/4 lg:w-2/4 xl:w-auto enter-x"
           >
             <LoginForm />
-            <ForgetPasswordForm />
-            <RegisterForm />
+            <!-- <ForgetPasswordForm />
+            <RegisterForm /> -->
             <MobileForm />
             <QrCodeForm />
             <TenantLoginForm />
@@ -51,8 +51,8 @@
   import { AppLogo } from '/@/components/Application';
   import { AppLocalePicker, AppDarkModeToggle } from '/@/components/Application';
   import LoginForm from './LoginForm.vue';
-  import ForgetPasswordForm from './ForgetPasswordForm.vue';
-  import RegisterForm from './RegisterForm.vue';
+  // import ForgetPasswordForm from './ForgetPasswordForm.vue';
+  // import RegisterForm from './RegisterForm.vue';
   import MobileForm from './MobileForm.vue';
   import QrCodeForm from './QrCodeForm.vue';
   import TenantLoginForm from './TenantLoginForm.vue';
